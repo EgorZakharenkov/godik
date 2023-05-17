4bbb40b02777dbe01f70c7a40c98f43dc4f147ca
EgorZakharenkov <egorboom2017@gmail.com>
2023-05-17 12:21:41 +0300

    new
    new

---
+++ app.js
@@ -0,0 +1,35 @@
+function IMG_01 (){
+    let img_1 = document.querySelector(".img_01")
+    img_1.style.display = "block";
+    document.querySelector(".btn_1").style.display = "none"
+}
+function IMG_02 (){
+    let img_2 = document.querySelector(".img_02")
+    img_2.style.display = "block";
+    document.querySelector(".btn_2").style.display = "none"
+
+}
+function IMG_03 (){
+    let img_03 = document.querySelector(".img_03")
+    img_03.style.display = "block";
+    document.querySelector(".btn_3").style.display = "none"
+
+}
+function IMG_04 (){
+    let img_04 = document.querySelector(".img_04")
+    img_04.style.display = "block";
+    document.querySelector(".btn_4").style.display = "none"
+
+}
+function IMG_05 (){
+    let img_05 = document.querySelector(".img_05")
+    img_05.style.display = "block";
+    document.querySelector(".btn_5").style.display = "none"
+
+}
+function IMG_06 (){
+    let img_06 = document.querySelector(".img_06")
+    img_06.style.display = "block";
+    document.querySelector(".btn_6").style.display = "none"
+
+}
